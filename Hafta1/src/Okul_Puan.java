25a57962f8b5949d2d488402d7d4bf250eeaf116
Merve Kahraman <edmer1662@gmail.com>
2022-09-21 23:46:44 +0300

    First Commit

---
+++ Hafta1/src/Okul_Puan.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Okul_Puan {
+    public static void main(String[] args) {
+        Scanner k=new Scanner(System.in);
+        int not;
+        System.out.print("Not Giriniz:");
+        not=k.nextInt();
+        if(not>=0 &&not <=100) {
+            if (not > 0 && not <=35) {
+                System.out.println("FF");
+            } else if (not >35 && not <=50) {
+                System.out.println("DC");
+            } else if (not > 50 && not <=70) {
+                System.out.println("BB");
+            } else {
+                System.out.println("AA");
+            }
+        }
+        else
+            System.out.println("Geçersiz Not Girdiniz");
+
+
+    }
+}
