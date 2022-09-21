746b3d80cf2fcd5028adc33e7b690f8456a42ac2
Merve Kahraman <edmer1662@gmail.com>
2022-09-21 23:59:41 +0300

    Refactor

--- Hafta1/src/Okul_Puan.java
+++ Hafta1/src/Okul_Puan.java
@@ -6,18 +6,17 @@ public class Okul_Puan {
         int not;
         System.out.print("Not Giriniz:");
         not=k.nextInt();
-        if(not>=0 &&not <=100) {
+
             if (not > 0 && not <=35) {
                 System.out.println("FF");
             } else if (not >35 && not <=50) {
                 System.out.println("DC");
             } else if (not > 50 && not <=70) {
                 System.out.println("BB");
-            } else {
+            } else if (not > 70 && not <= 100) {
                 System.out.println("AA");
             }
-        }
-        else
+            else
             System.out.println("Geçersiz Not Girdiniz");
 
 
